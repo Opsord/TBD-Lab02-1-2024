fa2655ccfd492b171ac5c51a335930084bfba7cc
Andres Zelaya <andres.zelaya.d@hotmail.com>
2024-05-03 18:49:35 -0400

    Remocion Sql2o
    Se remueven restos de Sql2o

--- LAB-TBD/src/main/java/G1TBD/LABTBD/config/DatabaseContext.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/config/DatabaseContext.java
@@ -1,9 +1,7 @@
 package G1TBD.LABTBD.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseContext {
@@ -15,9 +13,4 @@ public class DatabaseContext {
 
     @Value("${spring.datasource.password}")
     private String dbPassword;
-
-    @Bean
-    public Sql2o sql2o(){
-        return new Sql2o(dbUrl,dbUser,dbPassword);
-    }
 }
